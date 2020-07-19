61f0e8e20261c64f65b137c459a4bdbb529e0d16
Dan Gelok <dgelok@gmail.com>
2020-07-19 18:33:38 -0500

    all instructions except developing home page

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 
+// ejs
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
+// public
+app.use(express.static('public'))
+
+// routes
+app.use(require('./routes/index'))
+app.use(require('./routes/albums'))
+
+// views
 
 
 app.listen(4000, ()=>{
