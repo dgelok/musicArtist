8ec161758c654a358141e94819d446fe47bc9686
Dan Gelok <dgelok@gmail.com>
2020-07-20 20:15:53 -0500

    responsive album menu finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { response } = require('express');
 const router = express.Router();
+const dataFile = require('../data/data.json')
+
 
 router.get('/', (req, res)=>{
-    res.render('index.ejs')
+    
+    let albums = dataFile.albums
+    console.log(albums)
+    res.render('index.ejs', {
+        albums: albums
+    })
+
+
 })
 
 
